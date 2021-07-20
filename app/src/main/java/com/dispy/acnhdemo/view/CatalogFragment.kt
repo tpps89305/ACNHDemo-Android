0865819fcdd900fd8e4fcf9830d3b25d2e309429
Dispy-Desktop <tpps89305@hotmail.com>
2021-07-20 22:34:05 +0800

    Add catalog page

--- app/src/main/java/com/dispy/acnhdemo/view/CatalogFragment.kt
+++ app/src/main/java/com/dispy/acnhdemo/view/CatalogFragment.kt
@@ -4,12 +4,11 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dispy.acnhdemo.R
-import com.dispy.acnhdemo.dummy.DummyContent
+import com.dispy.acnhdemo.databinding.FragmentCatalogBinding
 
 /**
  * A fragment representing a list of Items.
@@ -18,45 +17,20 @@ class CatalogFragment : Fragment() {
 
     private var columnCount = 1
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_catalog, container, false)
+        val binding = FragmentCatalogBinding.inflate(layoutInflater)
 
-        // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
-                }
-                adapter = CatalogRecyclerViewAdapter(DummyContent.ITEMS)
+        with(binding.listCatalog) {
+            layoutManager = when {
+                columnCount <= 1 -> LinearLayoutManager(context)
+                else -> GridLayoutManager(context, columnCount)
             }
+            adapter = CatalogRecyclerViewAdapter(resources.getStringArray(R.array.catalog_items))
         }
-        return view
-    }
 
-    companion object {
-
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
-
-        // TODO: Customize parameter initialization
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-            CatalogFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_COLUMN_COUNT, columnCount)
-                }
-            }
+        return binding.root
     }
 }
--- app/src/main/java/com/dispy/acnhdemo/view/CatalogRecyclerViewAdapter.kt
+++ app/src/main/java/com/dispy/acnhdemo/view/CatalogRecyclerViewAdapter.kt
@@ -2,10 +2,8 @@ package com.dispy.acnhdemo.view
 
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import com.dispy.acnhdemo.R
+import com.dispy.acnhdemo.databinding.ItemCatalogBinding
 
 import com.dispy.acnhdemo.dummy.DummyContent.DummyItem
 
@@ -14,29 +12,41 @@ import com.dispy.acnhdemo.dummy.DummyContent.DummyItem
  * TODO: Replace the implementation with code for your data type.
  */
 class CatalogRecyclerViewAdapter(
-    private val values: List<DummyItem>
+    private val values: Array<String>
 ) : RecyclerView.Adapter<CatalogRecyclerViewAdapter.ViewHolder>() {
 
+    private lateinit var listener: OnItemClickListener
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_catalog, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(
+            ItemCatalogBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
-        holder.idView.text = item.id
-        holder.contentView.text = item.content
+        holder.bind(item)
     }
 
     override fun getItemCount(): Int = values.size
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val idView: TextView = view.findViewById(R.id.item_number)
-        val contentView: TextView = view.findViewById(R.id.content)
-
-        override fun toString(): String {
-            return super.toString() + " '" + contentView.text + "'"
+    inner class ViewHolder(private val binding: ItemCatalogBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+        fun bind(itemName: String) {
+            binding.itemName = itemName
+            binding.executePendingBindings()
         }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
     }
 }
