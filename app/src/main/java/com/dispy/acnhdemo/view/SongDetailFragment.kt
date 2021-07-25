9d68b517ebdf256182ab126f5f4f1a8d46b1bd43
Dispy-Desktop <tpps89305@hotmail.com>
2021-07-25 20:57:52 +0800

    Add function to play music

--- app/src/main/java/com/dispy/acnhdemo/view/SongDetailFragment.kt
+++ app/src/main/java/com/dispy/acnhdemo/view/SongDetailFragment.kt
@@ -1,5 +1,7 @@
 package com.dispy.acnhdemo.view
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.navArgs
+import com.dispy.acnhdemo.R
 import com.dispy.acnhdemo.databinding.FragmentSongDetailBinding
 import com.dispy.acnhdemo.viewmodel.SongDetailViewModel
 
@@ -14,6 +17,7 @@ class SongDetailFragment : Fragment() {
 
     private val args: SongDetailFragmentArgs by navArgs()
     private val viewModel = SongDetailViewModel()
+    private var mediaPlayer : MediaPlayer? = MediaPlayer()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,7 +32,39 @@ class SongDetailFragment : Fragment() {
         (activity as AppCompatActivity).supportActionBar?.title  = args.song.name.nameTWzh
         viewModel.getData(args.song)
 
+        initMusicPlayer()
+        binding.buttonPlay.setOnClickListener {
+            with(mediaPlayer!!) {
+                if (isPlaying) {
+                    stop()
+                    binding.buttonPlay.text = resources.getString(R.string.play)
+                } else {
+                    prepare()
+                    start()
+                    binding.buttonPlay.text = resources.getString(R.string.stop)
+                }
+            }
+        }
+
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+
+    private fun initMusicPlayer() {
+        mediaPlayer?.apply {
+            setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .build()
+            )
+            setDataSource(args.song.musicURI)
+        }
     }
 
 }
